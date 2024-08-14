c1ca8f349bc8159066a749d25519fd2c255fde7f
Karsten Jepsen <kjepsen86@gmail.com>
2024-08-14 14:28:58 -0500

    added Haptics

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { useState, useEffect } from "react";
 import { getFromStorage, saveToStorage } from "../utils/storage";
+import * as Haptics from "expo-haptics";
 
 const storageKey = "shopping-list";
 
@@ -47,12 +48,18 @@ export default function App() {
   const handleDelete = (id: string) => {
     const newShoppingList = shoppingList.filter((item) => item.id !== id);
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     setShoppingList(newShoppingList);
   };
 
   const handleToggleComplete = (id: string) => {
     const newShoppingList = shoppingList.map((item) => {
       if (item.id === id) {
+        if (item.completedAtTimestamp) {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+        } else {
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        }
         return {
           ...item,
           completedAtTimestamp: item.completedAtTimestamp
--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,7 +1,7 @@
 import { TouchableOpacity, View, Alert, StyleSheet, Text, Pressable } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import Entypo from "@expo/vector-icons/Entypo";
 import { theme } from "../theme";
+import * as Haptics from "expo-haptics";
 
 type Props = {
     name: string;
@@ -12,6 +12,7 @@ type Props = {
 
 export function ShoppingListItem({ name, isCompleted, onDelete, onToggleComplete }: Props) {
     const handleDelete = () => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
         Alert.alert(
             `Are you sure you want to delete ${name}?`,
             "It will be gone for good",
